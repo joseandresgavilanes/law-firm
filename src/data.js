efef468e2591ad1428c5db7126681ff0dc2ba30d
smiley_12_2 <joseandresgavilanes2012@gmail.com>
2022-10-02 14:35:46 -0500

    favicon

--- src/data.js
+++ src/data.js
@@ -1,67 +1,67 @@
 // import profile images
-import ProfilePhoto1 from '../src/assets/img/profile1.png';
-import ProfilePhoto2 from '../src/assets/img/profile2.png';
-import ProfilePhoto3 from '../src/assets/img/profile3.png';
+import ProfilePhoto1 from "../src/assets/img/profile1.png";
+import ProfilePhoto2 from "../src/assets/img/profile2.png";
+import ProfilePhoto3 from "../src/assets/img/profile3.png";
 
 // import about icons
-import Skill1 from '../src/assets/img/skills/skill1.png';
-import Skill2 from '../src/assets/img/skills/skill2.png';
-import Skill3 from '../src/assets/img/skills/skill3.png';
-import Skill4 from '../src/assets/img/skills/skill4.png';
-import Skill5 from '../src/assets/img/skills/skill5.png';
-import Skill6 from '../src/assets/img/skills/skill6.png';
+import Skill1 from "../src/assets/img/skills/skill1.png";
+import Skill2 from "../src/assets/img/skills/skill2.png";
+import Skill3 from "../src/assets/img/skills/skill3.png";
+import Skill4 from "../src/assets/img/skills/skill4.png";
+import Skill5 from "../src/assets/img/skills/skill5.png";
+import Skill6 from "../src/assets/img/skills/skill6.png";
 
 // import team images
-import TeamMember1 from '../src/assets/img/lawyer1.png';
-import TeamMember2 from '../src/assets/img/lawyer2.png';
-import TeamMember3 from '../src/assets/img/lawyer3.png';
+import TeamMember1 from "../src/assets/img/lawyer1.png";
+import TeamMember2 from "../src/assets/img/lawyer2.png";
+import TeamMember3 from "../src/assets/img/lawyer3.png";
 
 // import social icons
-import FacebookIcon from '../src/assets/img/social/facebook.png';
-import TwitterIcon from '../src/assets/img/social/twitter.png';
-import InstagramIcon from '../src/assets/img/social/instagram.png';
-import YoutubeIcon from '../src/assets/img/social/youtube.png';
-import PinterestIcon from '../src/assets/img/social/pinterest.png';
+import FacebookIcon from "../src/assets/img/social/facebook.png";
+import TwitterIcon from "../src/assets/img/social/twitter.png";
+import InstagramIcon from "../src/assets/img/social/instagram.png";
+import YoutubeIcon from "../src/assets/img/social/youtube.png";
+import PinterestIcon from "../src/assets/img/social/pinterest.png";
 
 // navigation data
 export const navigation = [
   {
-    name: 'Home',
-    href: 'home',
+    name: "Home",
+    href: "home",
   },
   {
-    name: 'What we do',
-    href: 'skills',
+    name: "What we do",
+    href: "skills",
   },
   {
-    name: 'Our team',
-    href: 'team',
+    name: "Our team",
+    href: "team",
   },
   {
-    name: 'Contact us',
-    href: 'contact',
+    name: "Contact us",
+    href: "contact",
   },
 ];
 
 // testimonials data
 export const testimonials = [
   {
-    text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem rem ad consequuntur similique sequi illum dolorum omnis inventore labore odio!',
-    name: 'Jane Cooper',
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem rem ad consequuntur similique sequi illum dolorum omnis inventore labore odio!",
+    name: "Jane Cooper",
     image: ProfilePhoto1,
-    position: 'Web Developer',
+    position: "Web Developer",
   },
   {
-    text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem rem ad consequuntur similique sequi illum dolorum omnis inventore labore odio!',
-    name: 'Theresa Webb',
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem rem ad consequuntur similique sequi illum dolorum omnis inventore labore odio!",
+    name: "Theresa Webb",
     image: ProfilePhoto2,
-    position: 'Web Designer',
+    position: "Web Designer",
   },
   {
-    text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem rem ad consequuntur similique sequi illum dolorum omnis inventore labore odio!',
-    name: 'Kathryn Murphy',
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem rem ad consequuntur similique sequi illum dolorum omnis inventore labore odio!",
+    name: "Kathryn Murphy",
     image: ProfilePhoto3,
-    position: 'UI/UX Designer',
+    position: "UI/UX Designer",
   },
 ];
 
@@ -69,39 +69,39 @@ export const testimonials = [
 export const skills = [
   {
     icon: Skill1,
-    title: 'Corruption',
+    title: "Corruption",
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.',
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.",
   },
   {
     icon: Skill2,
-    title: 'Employment',
+    title: "Employment",
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.',
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.",
   },
   {
     icon: Skill3,
-    title: 'Real Estate',
+    title: "Real Estate",
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.',
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.",
   },
   {
     icon: Skill4,
-    title: 'Taxes',
+    title: "Taxes",
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.',
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.",
   },
   {
     icon: Skill5,
-    title: 'Corporate',
+    title: "Corporate",
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.',
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.",
   },
   {
     icon: Skill6,
-    title: 'Banking',
+    title: "Banking",
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.',
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.",
   },
 ];
 
@@ -109,24 +109,24 @@ export const skills = [
 export const team = [
   {
     image: TeamMember1,
-    name: 'Jane William',
-    position: 'Band & Financial Lawyer',
+    name: "Jane William",
+    position: "Bank & Financial Lawyer",
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.',
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.",
   },
   {
     image: TeamMember2,
-    name: 'Andrea Collins',
-    position: 'Family Lawyer',
+    name: "Andrea Collins",
+    position: "Family Lawyer",
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.',
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.",
   },
   {
     image: TeamMember3,
-    name: 'John Smith',
-    position: 'Business Lawyer',
+    name: "John Smith",
+    position: "Business Lawyer",
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.',
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed erat nibh tristique ipsum.",
   },
 ];
 
